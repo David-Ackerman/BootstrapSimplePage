8e2aa1caadf2020679709a79896cbbee338b5af9
david santos <david_pereira-16@hotmail.com>
2020-08-12 15:24:59 -0300

    Commit final

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+/**
+ * Versão a partir de 19/12/2019
+ * Este arquivo contêm a nova forma de montar o arquivo de configuração do gulp
+ * o gulpfile.js.
+ */
+
+/*
+ * Variables (Variáveis)
+ */
+
+// Require (Requisições)
+const {series, src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+// Sass Bootstrap Source (Código Sass Bootstrap )
+const scssBootstrap = './node_modules/bootstrap/scss/bootstrap.scss';
+
+// Sass Source (Código Sass)
+const scssFiles = './src/scss/*.scss';
+
+// CSS destination (Destino do CSS)
+const cssDest = './src/css';
+
+// JS destination (Destino do JS)
+const jsDest = './src/js';
+
+// jQuery (npm install jquery)
+const jquery =  './node_modules/jquery/dist/jquery.min.js';
+
+// Bootstrap JS (bootstrap.bundle.min.js já vem com o popper.js)
+const bootstrap_js = './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Options for development (Opções para o ambiente de desenvolvimento)
+const sassDevOptions = {
+    outputStyle: 'expanded'
+}
+
+// Options for production (Opções para o ambiente de produção)
+const sassProdOptions = {
+    outputStyle: 'compressed'
+}
+
+// Task 'js' - Run with command 'gulp js' (Comando executado com 'gulp js') - Carrego os javascripts
+function js(cb){
+    src([jquery,bootstrap_js]).pipe(dest(jsDest));
+    cb();
+}
+
+//atribuo um nome para a função poder ser chamada na linha de comando
+exports.js = js;
+
+// Task 'sassdev' - Run with command 'gulp sassdev' (Comando executado com 'gulp sassdev')
+function sassdev(cb) {
+    src([scssBootstrap,scssFiles])
+        .pipe(sass(sassDevOptions).on('error', sass.logError))
+        .pipe(dest(cssDest));
+    cb();
+}
+
+//atribuo um nome para a função poder ser chamada na linha de comando
+exports.sassdev = sassdev;
+
+// Task 'sassprod' - Run with command 'gulp sassprod' (Comando executado com 'gulp sassprod')
+function sassprod(cb) {
+      src([scssBootstrap,scssFiles])
+        .pipe(sass(sassProdOptions).on('error', sass.logError))
+        .pipe(dest(cssDest));
+      cb();
+}
+
+//atribuo um nome para a função poder ser chamada na linha de comando
+exports.sassprod = sassprod;
+
+function monitor() {
+    browserSync.init({
+        server: {
+           baseDir: "./src",
+           index: "/index.html"
+        }
+    });
+    watch('./src/scss/*.scss', sassprod);
+    watch('./src/*.html').on('change',browserSync.reload);
+    watch('./src/js/*.js').on('change', browserSync.reload);
+}
+exports.monitor = monitor;
+//Executo todas as tasks de uma vez utilizando esta função do gulp exatamente nesta ordem
+exports.default = series(js,sassdev,sassprod, monitor);
